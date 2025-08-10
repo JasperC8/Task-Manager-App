41cbb5a17c70926425b4714c370fceb326decfb5
Jasper Chan <jasperc614@gmail.com>
2025-08-10 19:05:26 +1000

    EPRE-5 User Story 2: Role-Based Access Control

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
-const PORT = process.env.PORT || 5001;
+app.use('/api/prescriptions', require('./routes/prescriptionRoutes'));
 
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
