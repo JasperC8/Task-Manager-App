ca39c58631c921d52e0ff2ada7ee731f513793cb
Jasper Chan <jasperc614@gmail.com>
2025-08-12 16:45:41 +1000

    EPRE-23 Frontend design Auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Doctor from "./pages/doctor"; 
+import Pharmacy from "./pages/pharmacy"; 
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/doctor"
+            element={
+              <ProtectedRoute allow={["doctor"]}>
+                <Doctor />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/pharmacy"
+            element={
+              <ProtectedRoute allow={["pharmacy"]}>
+                <Pharmacy />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
-
-export default App;
