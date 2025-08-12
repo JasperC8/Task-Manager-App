60bb07f5536ad8493de63fb0e8dceeceb04fcd5a
Jasper Chan <jasperc614@gmail.com>
2025-08-12 19:08:50 +1000

    error patch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+axios.interceptors.response.use(
+  (r) => r,
+  (err) => {
+    const status = err?.response?.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem("token");
+      delete axios.defaults.headers.common.Authorization;
+      window.location = "/login";
+    }
+    return Promise.reject(err);
+  }
 );
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
