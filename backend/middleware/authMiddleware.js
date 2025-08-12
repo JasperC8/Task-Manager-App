3dc86a484376dd4821b75ab31f2dcf688e008c04
Jasper Chan <jasperc614@gmail.com>
2025-08-12 23:25:55 +1000

    Typo fixed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const protect = (roles = []) => {
       try {
         token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded._id).select('-password');
+        const user = await User.findById(decoded.id).select('-password');
 
         if (!user || (roles.length && !roles.includes(user.role))) {
           return res.status(403).json({ message: 'Forbidden' });
